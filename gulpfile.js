7e76e73a4a96ca20b2503fefa1737244c9ae8312
Matt Sloan <msloan@knotice.com>
2015-10-01 15:40:00 -0400

    added in gulp-stylus task to convert/minify stylus to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var rename = require('gulp-rename');
 
-gulp.task('default', ['templates', 'js'], function() {
+gulp.task('default', ['templates', 'js', 'css'], function() {
   // place code for your default task here
   console.log('gulp process completed');
 });
@@ -26,4 +28,14 @@ gulp.task('js', function() {
 
   gulp.src('src/client/libs/**/*.js')
     .pipe(gulp.dest('dist/libs'));
+});
+
+gulp.task('css', function() {
+  //compile stylus file assets into one minimized css file
+  gulp.src('src/client/css/**/*.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('dist/css'));
 });
