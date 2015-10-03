6cd2ea988b3ee900ce23d1a6667ca00404d4f4ae
Matt Sloan <msloan@knotice.com>
2015-10-02 20:50:41 -0400

    added dynamic page titles, simple page animations using angular-animate.

--- src/client/angular/portfolio/portfolio.ctrl.js
+++ src/client/angular/portfolio/portfolio.ctrl.js
@@ -1,10 +1,14 @@
 angular.module('PortfolioCtrl', []).controller('PortfolioController', function($scope, $http) {
 
-   $scope.title = 'Portfolio';  
+  $scope.title = 'Portfolio';  
 
-   $http.get('api/portfolio')
+  $http.get('api/portfolio')
     .success(function (res) {
       $scope.portfolioList = res;
     });
 
+  $scope.$on('$routeChangeSuccess', function (event, data) {
+    $scope.errormessage = '';
+    $scope.pageTitle = data.title + ' | Matt Sloan | UI Designer &amp; Full-Stack Developer';
+  });
 });
